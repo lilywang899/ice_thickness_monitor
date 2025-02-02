6e967fc8611aecf211c965518d8e338c4593f896
Lily W <lily.jingyi.wang@gmail.com>
2025-02-02 00:36:07 -0500

    added multithreading to read and send sensor data

--- IoT_temp_sensor/esp32_ice_thickness.ino
+++ IoT_temp_sensor/esp32_ice_thickness.ino
@@ -27,19 +27,18 @@ extern "C" {
 
 // Digital pin connected to the DHT sensor
 #define DHTPIN 4  
-//#define DHT11PIN 4
 
-// Uncomment whatever DHT sensor type you're using
-#define DHTTYPE DHT11   // DHT 11 lily
-//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)   
+#define DHTTYPE DHT11   
+
+#define USE_MUTEX
+SemaphoreHandle_t mutex;
 
 // Initialize DHT sensor
 DHT dht(DHTPIN, DHTTYPE); 
 
-// Variables to hold sensor readings
-float temp;
-float hum;
+// Variables to hold sensor readings, shared_variables
+float temp = 0.0;
+float hum = 0.0;
 
 AsyncMqttClient mqttClient;
 TimerHandle_t mqttReconnectTimer;
@@ -57,27 +56,6 @@ void connectToMqtt() {
   Serial.println("Connecting to MQTT...");
   mqttClient.connect();
 }
-
-// void WiFiEvent(WiFiEvent_t event) {
-//   Serial.printf("[WiFi-event] event: %d\n", event);
-//   switch(event) {
-//     case ARDUINO_EVENT_WIFI_STA_GOT_IP:
-//       Serial.println("WiFi connected");
-//       Serial.println("IP address: ");
-//       Serial.println(WiFi.localIP());
-//       connectToMqtt();
-//       break;
-//     case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
-//       Serial.println("WiFi lost connection");
-//       xTimerStop(mqttReconnectTimer, 0); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
-//       xTimerStart(wifiReconnectTimer, 0);
-//       break;
-//     case IP_EVENT_STA_GOT_IP:
-//       Serial.print("IP Address: ");
-//       Serial.println(WiFi.localIP());
-//       break;
-//   }
-// }
 
 void WiFiEvent(WiFiEvent_t event) {
   switch(event) {
@@ -131,6 +109,65 @@ void onMqttPublish(uint16_t packetId) {
   Serial.println(packetId);
 }
 
+void Read(void *pvParameters) {  //Read sensor task
+    printf("Task 0          | Task 1\n");
+    //reading sensor data
+    float new_hum;
+    float new_temp;
+    unsigned long currentMillis = millis();
+    // Every X number of seconds (interval = 10 seconds) 
+    // it publishes a new MQTT message
+    if (currentMillis - previousMillis >= interval) {
+      // Save the last time a new reading was published
+      previousMillis = currentMillis;
+      // New DHT sensor readings
+      new_hum = dht.readHumidity();
+      Serial.printf("hum: %.2f \n", hum);
+      // Read temperature as Celsius (the default)
+      new_temp = dht.readTemperature();
+      Serial.printf("temp: %.2f \n", temp);
+      // Read temperature as Fahrenheit (isFahrenheit = true)
+      //temp = dht.readTemperature(true);
+
+      // Check if any reads failed and exit early (to try again).
+      if (isnan(temp) || isnan(hum)) {
+        Serial.println(F("Failed to read from DHT sensor!"));
+        return; 
+      }
+    }
+    while (1) {
+        xSemaphoreTake(mutex, portMAX_DELAY);  // Lock access to shared_variable
+
+        printf(" hum %.2f <- %.2f, temp %.2f <- %.2f     |\n", hum, new_hum, temp, new_temp);
+        hum = new_hum;
+        temp = new_temp;
+
+        xSemaphoreGive(mutex);  // Unlock access to shared_variable
+        vTaskDelay(pdMS_TO_TICKS(1000));  // Simulate work
+    }
+}
+
+void Send(void *pvParameters) {  // Send data task
+    printf("                 | Starting\n");
+    while (1) {
+        xSemaphoreTake(mutex, portMAX_DELAY);  // Lock access to shared_variable
+
+        printf("                                  | R_hum: %.2f, R_temp: %.2f\n", hum, temp);  // Only reading
+
+        // Publish an MQTT message on topic esp32/dht/temperature
+        uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_TEMP, 1, true, String(temp).c_str());                            
+        Serial.printf("Publishing on topic %s at QoS 1, packetId: %i", MQTT_PUB_TEMP, packetIdPub1);
+        Serial.printf("Message: %.2f \n", temp);
+
+        // Publish an MQTT message on topic esp32/dht/humidity
+        uint16_t packetIdPub2 = mqttClient.publish(MQTT_PUB_HUM, 1, true, String(hum).c_str());                            
+        Serial.printf("Publishing on topic %s at QoS 1, packetId %i: ", MQTT_PUB_HUM, packetIdPub2);
+        Serial.printf("Message: %.2f \n", hum);
+        xSemaphoreGive(mutex);  // Unlock access to shared_variable
+        vTaskDelay(pdMS_TO_TICKS(500));  // Simulate work
+    }
+}
+
 void setup() {
   Serial.begin(115200);
   Serial.println();
@@ -151,40 +188,71 @@ void setup() {
   // If your broker requires authentication (username and password), set them below
   //mqttClient.setCredentials("REPlACE_WITH_YOUR_USER", "REPLACE_WITH_YOUR_PASSWORD");
   connectToWifi();
+
+  Serial.printf(" Reading          | Sending\n");
+
+  #ifdef USE_MUTEX
+    mutex = xSemaphoreCreateMutex();  // Create the mutex
+  #endif
+
+  static int task_number0 = 0;
+  xTaskCreate(
+    Read, "Reading"  // A name just for humans
+    ,
+    2048  // The stack size
+    ,
+    (void *)&task_number0  // Pass reference to a variable describing the task number
+    //,  5  // High priority
+    ,
+    1  // priority
+    ,
+    NULL  // Task handle is not used here - simply pass NULL
+  );
+
+  static int task_number1 = 1;
+  xTaskCreate(
+    Send, "Sending", 2048  // Stack size
+    ,
+    (void *)&task_number1  // Pass reference to a variable describing the task number
+    ,
+    1  // Low priority
+    ,
+    NULL  // Task handle is not used here - simply pass NULL
+  );
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-  // Every X number of seconds (interval = 10 seconds) 
-  // it publishes a new MQTT message
-  if (currentMillis - previousMillis >= interval) {
-    // Save the last time a new reading was published
-    previousMillis = currentMillis;
-    temp = 100;
-    hum = 200;
-    // New DHT sensor readings
-    hum = dht.readHumidity();
-    Serial.printf("hum: %.2f \n", hum);
-    // Read temperature as Celsius (the default)
-    temp = dht.readTemperature();
-    Serial.printf("temp: %.2f \n", temp);
-    // Read temperature as Fahrenheit (isFahrenheit = true)
-    //temp = dht.readTemperature(true);
+  // unsigned long currentMillis = millis();
+  // // Every X number of seconds (interval = 10 seconds) 
+  // // it publishes a new MQTT message
+  // if (currentMillis - previousMillis >= interval) {
+  //   // Save the last time a new reading was published
+  //   previousMillis = currentMillis;
+  //   temp = 100;
+  //   hum = 200;
+  //   // New DHT sensor readings
+  //   hum = dht.readHumidity();
+  //   Serial.printf("hum: %.2f \n", hum);
+  //   // Read temperature as Celsius (the default)
+  //   temp = dht.readTemperature();
+  //   Serial.printf("temp: %.2f \n", temp);
+  //   // Read temperature as Fahrenheit (isFahrenheit = true)
+  //   //temp = dht.readTemperature(true);
 
-    // Check if any reads failed and exit early (to try again).
-    if (isnan(temp) || isnan(hum)) {
-      Serial.println(F("Failed to read from DHT sensor!"));
-      //return; lily
-    }
+  //   // Check if any reads failed and exit early (to try again).
+  //   if (isnan(temp) || isnan(hum)) {
+  //     Serial.println(F("Failed to read from DHT sensor!"));
+  //     //return; lily
+  //   }
     
-    // Publish an MQTT message on topic esp32/dht/temperature
-    uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_TEMP, 1, true, String(temp).c_str());                            
-    Serial.printf("Publishing on topic %s at QoS 1, packetId: %i", MQTT_PUB_TEMP, packetIdPub1);
-    Serial.printf("Message: %.2f \n", temp);
+  //   // Publish an MQTT message on topic esp32/dht/temperature
+  //   uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_TEMP, 1, true, String(temp).c_str());                            
+  //   Serial.printf("Publishing on topic %s at QoS 1, packetId: %i", MQTT_PUB_TEMP, packetIdPub1);
+  //   Serial.printf("Message: %.2f \n", temp);
 
-    // Publish an MQTT message on topic esp32/dht/humidity
-    uint16_t packetIdPub2 = mqttClient.publish(MQTT_PUB_HUM, 1, true, String(hum).c_str());                            
-    Serial.printf("Publishing on topic %s at QoS 1, packetId %i: ", MQTT_PUB_HUM, packetIdPub2);
-    Serial.printf("Message: %.2f \n", hum);
-  }
+  //   // Publish an MQTT message on topic esp32/dht/humidity
+  //   uint16_t packetIdPub2 = mqttClient.publish(MQTT_PUB_HUM, 1, true, String(hum).c_str());                            
+  //   Serial.printf("Publishing on topic %s at QoS 1, packetId %i: ", MQTT_PUB_HUM, packetIdPub2);
+  //   Serial.printf("Message: %.2f \n", hum);
+  // }
 }
